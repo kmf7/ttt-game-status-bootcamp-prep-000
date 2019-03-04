09e0e90b635b7de03a8b99f6ce5e2b4d393ad900
Kimberly Fritter <littlekimmy44@hotmail.com>
2019-03-04 16:21:45 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,14 +7,13 @@ end
 WIN_COMBINATIONS= [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
 
+
+
 def won?(board)
-  WIN_COMBINATIONS.each do |i|
-    if i.all?{|index| board[index] == "X"}
-      return i
-    elsif i.all?{|index| board[index] == "O"}
-     return i 
-    else
-      false
+  WIN_COMBINATIONS.each do |combo|
+    if combo.all? { |index| board[index] == "X"} || combo.all? { |index| board[index] == "O"}
+      return combo
     end
   end
+  return nil
 end
